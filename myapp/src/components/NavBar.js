a2d1c4953c2a5001d0d7e2774158b2d17fe6c397
ARodriguez <rodriguez.antonella.n@gmail.com>
2022-01-24 18:15:41 -0300

    Added NavBar

---
+++ myapp/src/components/NavBar.js
@@ -0,0 +1,25 @@
+import "./NavBar.css";
+import { Nav, Container, Navbar } from "react-bootstrap"
+
+const NavBar = (props) => {
+    return (
+        <Navbar bg="dark" variant="dark" >
+            <Container>
+                <Navbar.Brand href="#home">My Store</Navbar.Brand>
+
+                <Nav class="Navigation"
+                    activeKey="/home"
+                    onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+                >
+                    <Nav.Link href="/home" className="linkText">Home</Nav.Link>
+                    <Nav.Link eventKey="link-1" className="linkText">About Us</Nav.Link>
+                    <Nav.Link eventKey="link-2" className="linkText">Products</Nav.Link>
+                    <Nav.Link eventKey="link-2" className="linkText">Contact</Nav.Link>
+
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavBar;
