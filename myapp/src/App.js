c6e234f540c76766f9e98cc866d727bc19898beb
ARodriguez <rodriguez.antonella.n@gmail.com>
2022-02-16 23:25:26 -0300

    Fix

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,14 @@
 // REACT ROUTER DOM
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppRouter } from "./routers";
 import React, { useState } from "react";
 
 // COMPONENTS
 import NavBar from "./components/NavBar";
 
 // VIEWS
-import Home from "./views/Home/Home";
-import About from "./views/About/About";
-import Contact from "./views/Contact/Contact";
+// import Home from "./views/Home/Home";
+// import About from "./views/About/About";
+// import Contact from "./views/Contact/Contact";
 import ItemDetailContainer from "../src/components/ItemDetailContainer";
 import Cart from "./components/Cart";
 import { ListProvider } from "./context/CartContext";
@@ -21,23 +21,21 @@ const App = (props) => {
   };
 
   return (
-    <ListProvider value={[]} initial={0} min={0} max={10}>
-      <Router>
-        <div>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route
-              path="/detail/:itemId"
-              element={<ItemDetailContainer itemToItemDetail={data} />}
-            />
-            <Route path="/cart/:amount" element={<Cart />} />
-          </Routes>
-        </div>
-      </Router>
-    </ListProvider>
+    <div>
+      <NavBar />
+      <AppRouter />
+      {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/detail/:itemId"
+            element={<ItemDetailContainer itemToItemDetail={data} />}
+          />
+          <Route path="/cart/:amount" element={<Cart />} />
+        </Routes>
+      </div> */}
+    </div>
   );
 };
 
--- myapp/src/components/NavBar.js
+++ myapp/src/components/NavBar.js
@@ -1,25 +1,26 @@
 import "./NavBar.css";
-import { Nav, Container, Navbar } from "react-bootstrap"
+import { Nav, Container, Navbar } from "react-bootstrap";
 import CartWidget from "./CartWidget";
-import { LinkContainer } from 'react-router-bootstrap'
 
 const NavBar = () => {
-    return (
-        <Navbar bg="dark" variant="dark" >
-            <Container>
-                <LinkContainer to='/'><Navbar.Brand>My Store</Navbar.Brand></LinkContainer>
-                <Nav className="Navigation"
-                    activeKey="/home"
-                >
-                    <LinkContainer to='/'><Nav.Link className="linkText">Home</Nav.Link></LinkContainer>
-                    <LinkContainer to='/about'><Nav.Link className="linkText">About Us</Nav.Link></LinkContainer>
-                    <LinkContainer to='/contact'><Nav.Link className="linkText">Contact</Nav.Link></LinkContainer>
-                    <Nav.Link eventKey="Cart" className="linkText"><CartWidget /></Nav.Link>
-
-                </Nav>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">My Store</Navbar.Brand>
+        <Nav className="Navigation" activeKey="/home">
+          <Nav.Link href="/" className="linkText">
+            Home
+          </Nav.Link>
+          <Nav.Link href="/About" className="linkText">
+            About Us
+          </Nav.Link>
+          <Nav.Link href="/Contact" className="linkText">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
